d48fcd2f1ab26f9ea779bc0a24d1d6fe3da64ede
KAAN YARDIMCI <kaan.yardimci@yahoo.com.tr>
2023-02-25 01:15:59 +0300

    navbar

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+"use strict";
+
+const closeBtn = document.querySelector("#close-btn");
+const navBtn = document.querySelector("#nav-btn");
+const navbar = document.querySelector("#navbar");
+
+navBtn.addEventListener("click", () => {
+  navbar.classList.remove("close-navbar");
+});
+
+closeBtn.addEventListener("click", () => {
+  navbar.classList.add("close-navbar");
+});
