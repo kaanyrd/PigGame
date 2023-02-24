4f1e87af97625a755336e0debc7c8d6202d497c9
KAAN YARDIMCI <kaan.yardimci@yahoo.com.tr>
2023-02-25 02:58:43 +0300

    JS

--- JS/app.js
+++ JS/app.js
@@ -14,20 +14,25 @@ closeBtn.addEventListener("click", () => {
 // END OF NAVBAR
 // GAME
 const diceEl = document.querySelector(".dice");
-const player1Score = document.querySelector(".player-1-score");
-const player2Score = document.querySelector(".player-2-score");
+let player1Score = document.querySelector(".player-1-score");
+let player2Score = document.querySelector(".player-2-score");
+let player1Current = document.querySelector(".player-1-current");
+let player2Current = document.querySelector(".player-2-current");
 const newBtn = document.querySelector("#new-btn");
 const holdBtn = document.querySelector("#hold-btn");
 const rollBtn = document.querySelector("#roll-btn");
 
-diceEl.classList.add("hidden");
+diceEl.style.opacity = "0";
 player1Score.textContent = 0;
 player2Score.textContent = 0;
+player1Current.textContent = 0;
+player2Current.textContent = 0;
 
+diceEl.style.transition = "all .1s ease-in-out";
 rollBtn.addEventListener("click", () => {
   let dice = Math.trunc(Math.random() * 6) + 1;
   diceEl.src = `/IMGs/dice-${dice}.png`;
-  diceEl.classList.remove("hidden");
+  diceEl.style.opacity = "1";
   console.log(dice);
 });
 // END OF GAME
